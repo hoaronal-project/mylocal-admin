f622f4414af968448cf84a056f9897a61a9d88df
Quang Hoa <project.devskill@gmail.com>
2022-03-03 02:00:35 +0700

    init

---
+++ config/routes.ts
@@ -0,0 +1,64 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    name: 'test sdsd',
+    icon: 'table',
+    path: '/test',
+    component: './test',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageContainer } from '@ant-design/pro-layout';
+
+const TableList: React.FC = () => {
+  return (
+    <PageContainer>
+      <span>hello</span>
+    </PageContainer>
+  );
+};
+
+export default TableList;
